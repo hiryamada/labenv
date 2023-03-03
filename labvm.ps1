ae202eb6742acbe2b0aec370ba633fdf22fc2bc2
hiryamada <71864722+hiryamada@users.noreply.github.com>
2023-03-03 23:53:12 +0900

    Update labvm.ps1

--- labvm.ps1
+++ labvm.ps1
@@ -1,12 +1,11 @@
 # install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# refresh
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+refreshenv
 
 # install packages
-choco install -y microsoft-edge azure-cli az.powershell vscode dotnetcore-sdk microsoftazurestorageexplorer setdefaultbrowser azure-functions-core-tools
+choco install -y chrome setdefaultbrowser azure-cli vscode dotnet-7.0-sdk git 
 
-# install httprepl
-& "C:\Program Files\dotnet\dotnet.exe" tool install -g Microsoft.dotnet-httprepl
-
-# set Edge as default browser
-# SetDefaultBrowser.exe HKLM "Microsoft Edge" delay=1000
-SetDefaultBrowser.exe edge
+SetDefaultBrowser chrome
